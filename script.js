34183b3e1415ee6e50fd8cd61e919bedc0eb5063
tomas <tomas.gentile@gm2dev.com>
2024-10-14 11:23:17 -0300

    feat(car): add constructor of cars

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ Vehicle.prototype.stop = function () {
 Vehicle.prototype.showInfo = function () {
   console.log(this.brand, this.model, this.maxSpeed);
 };
+
+function Car(brand, model, maxSpeed, doors) {
+  let vehicle = new Vehicle(brand, model, maxSpeed);
+  this.brand = vehicle.brand;
+  this.model = vehicle.model;
+  this.maxSpeed = vehicle.maxSpeed;
+  this.speed = vehicle.speed;
+
+  this.doors = doors;
+}
+
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructor = Car;
+
+Car.prototype.opendDoors = function () {
+  console.log("doors open");
+};
